4073f15ddf94f9c69f5167fb1bad9d9ba3ed747d
nha nguyen <nhvn>
2018-05-18 07:39:47 +0700

    chuck

--- bai48pipe.js
+++ bai48pipe.js
@@ -1,7 +1,15 @@
 var fs = require("fs");
+var zlib = require("zlib");
 var readable = fs.createReadStream(__dirname +"/readme.txt",{
     encoding:"utf8",
     highWaterMark:16 * 1024
 });
 var writeable = fs.createWriteStream(__dirname + "/readmecopy.txt");
+var compressed = fs.createWriteStream(__dirname + "/readme.txt.gz");
+var gzib = zlib.createGzip();
+
+//copy
 readable.pipe(writeable);
+
+//compress
+readable.pipe(gzib).pipe(compressed);
